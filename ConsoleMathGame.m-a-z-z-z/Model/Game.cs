984720d14a4b8de07b2328b6964d99515ca083a6
Dylan Maziarek <21125921+m-a-z-z-z@users.noreply.github.com>
2024-09-03 17:06:23 +0100

    Added Game class

---
+++ ConsoleMathGame.m-a-z-z-z/Model/Game.cs
@@ -0,0 +1,27 @@
+﻿namespace ConsoleMathGame.m_a_z_z_z.Model;
+
+internal class Game
+{
+    public int Score { get; set; }
+    public DateTime Date { get; set; }
+    public GameMode GameMode { get; set; }
+    public string PlayerName { get; set; }
+
+    internal Game(GameMode gameMode, int score, string date, string playerName)
+    {
+        GameMode = GameMode;
+        Score = score;
+        Date = DateTime.Parse(date);
+        PlayerName = playerName;
+    }
+
+    internal Game(GameMode gameMode) {  GameMode = gameMode; }
+}
+
+internal enum GameMode
+{
+    Addition,
+    Subtraction,
+    Multiplication,
+    Division
+}
